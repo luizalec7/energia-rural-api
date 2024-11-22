567f87ec3fd6c07068a43b9a8dd2cb22c238fa13
luiz alecsander viana <luiz.alecsander@gmail.com>
2024-11-21 23:00:19 -0300

    Corrige variável PORT no application.properties e adiciona logs de debug

--- src/main/java/com/energia/rural/energia_rural_api/EnergiaRuralApiApplication.java
+++ src/main/java/com/energia/rural/energia_rural_api/EnergiaRuralApiApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EnergiaRuralApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("====== Variáveis de Ambiente ======");
+		System.getenv().forEach((key, value) -> System.out.println(key + ": " + value));
 		SpringApplication.run(EnergiaRuralApiApplication.class, args);
 	}
-
 }
